cbbff5d5ca46c35f442b8b1c32a2ad7927f774a9
shreenathtechnologiess-dot <shreenathtechnologiess@gmail.com>
2025-09-13 10:10:37 +0530

    Delete pushtimargshringar directory

--- pushtimargshringar/partials/whatsapp.php
+++
@@ -1,9 +0,0 @@
-<?php
-// whatsapp.php
-?>
-
-<!-- Floating WhatsApp Button -->
-<a href="https://wa.me/919999999999" target="_blank"  
-   class="fixed bottom-6 right-6 w-14 h-14 rounded-full shadow-lg overflow-hidden">
-   <img src="<?= SITE_URL ?>/assets/images/icons/whatsapp.png" alt="WhatsApp" class="w-full h-full object-cover">
-</a>
